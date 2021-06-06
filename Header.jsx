3dae425b87ddf4695c782c9571942910e5484421
noorularfeen007 <85439142+noorularfeen007@users.noreply.github.com>
2021-06-06 16:56:52 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="header">LinkedIN</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Job Portal</h1>
+      <p>
+        LinkedIn is an American business and employment-oriented online service
+        that operates via websites and mobile apps. Launched on May 5, 2003, the
+        platform is mainly used for professional networking, and allows job
+        seekers to post their CVs and employers to post jobs.
+      </p>
+    </div>
+  );
+}
+export default Note;
